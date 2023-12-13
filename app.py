55afa94a7f0b3ed94d2b2d51c699b5e3f5bed920
AMINE IDELHAJ <84861819+amineidel1@users.noreply.github.com>
2023-12-13 19:51:21 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -42,23 +42,31 @@ def main():
     # Adding the explanatory section
     st.markdown("""
     ## About the App
-    To enhance your experience with our recommendation platform, we've incorporated a feature that not only suggests items you'll love but also explains why they're a good fit for you. Here's how it adds value to your experience:
+    You want to know why we have recomanded this song for you 🤔
 
-    This process is visualized in our user-friendly interface, where the 'User-item Interaction Matrix' shows your ratings for various items, like movies or books. Just as a chef uses ingredients to create a recipe, our system uses this matrix to distill your unique tastes and preferences. It then approximates this matrix by factoring it into two smaller matrices—'User Matrix' and 'Item Matrix'. These matrices highlight the underlying factors that connect your preferences to item attributes. When multiplied together, they reconstruct the original matrix, filling in the gaps with predicted ratings, suggesting new items you're likely to enjoy. This mathematical innovation powers the recommendations you see, providing transparent and personalized suggestions tailored just for you.
+    To enhance your experience with our recommendation platform, we've incorporated a feature that not only suggests items you'll love but also explains why they're a good fit for you. Here's how it adds value to your experience: 
 
+    This process is explained here, where the 'User-item Interaction Matrix' shows your ratings for various items(songs). Just as a chef uses ingredients to create a recipe, our system uses this matrix to distill your unique tastes and preferences. It then approximates this matrix by factoring it into two smaller matrices—'User Matrix' and 'Item Matrix'. These matrices highlight the underlying factors that connect your preferences to item attributes. When multiplied together, they reconstruct the original matrix, filling in the gaps with predicted ratings, suggesting new items you're likely to enjoy. This mathematical innovation powers the recommendations you see, providing transparent and personalized suggestions tailored just for you.
 
     The 'Why' Behind Recommendations: Our system uses a sophisticated algorithm called Singular Value Decomposition (SVD), which considers your past interactions to find patterns and similarities with other users and items. When it suggests something, it also provides a reason, such as 'Users like you enjoyed this item,' or 'This is similar to other items you've rated highly.'
 
-
     Our code is crafted with efficiency and maintainability in mind. We use the latest best practices to ensure that explanations are generated quickly and accurately reflect the recommendation logic.
 
-    We chose this method over others because it strikes the right balance between transparency and complexity. It gives you enough insight to trust and understand the recommendations without overwhelming you with technical details.
-    Our recommendation engine is like a digital mind-reader, scoring an impressive 2.21 out of 10 in predicting what you'll enjoy next. This means it's really good at guessing what you like, with just a tiny margin of error. It's all thanks to a clever math trick called SVD that helps us see patterns in what you and others enjoy. So sit back and let us find you your next favorite song- our system's pretty sharp at picking songs match your taste! 
+    We chose this method over others because it strikes the right balance between transparency and complexity. It gives you enough insight to trust and understand the recommendations without overwhelming you with technical details. 
 
-    With this feature, you not only receive personalized recommendations but also gain insight into how they were chosen, making your experience more transparent and trustworthy.
-    This process is explqined here , where the 'User-item Interaction Matrix' shows your ratings for various items, like movies or books. Just as a chef uses ingredients to create a recipe, our system uses this matrix to distill your unique tastes and preferences. It then approximates this matrix by factoring it into two smaller matrices—'User Matrix' and 'Item Matrix'. These matrices highlight the underlying factors that connect your preferences to item attributes. When multiplied together, they reconstruct the original matrix, filling in the gaps with predicted ratings, suggesting new items you're likely to enjoy. This mathematical innovation powers the recommendations you see, providing transparent and personalized suggestions tailored just for you.
+    Our recommendation engine is like a digital mind-reader, scoring an impressive 2.21 out of 10 in predicting what you'll enjoy next. This means it's good at guessing what you like, with just a tiny margin of error. It's all thanks to a clever math trick called SVD that helps us see patterns in what you and others enjoy. So, sit back and let us find you your next favorite song- our system's pretty sharp at picking songs match your taste! 😄
     """)
-    st.image('Graph\Test.png', width=300)
+    st.image('Test.png', width=800)
+    # Title or explanation for your video
+    # Display the title or explanation
+    st.markdown("""
+    ## A Step-by-Step Guide""")
+    # Path to the video file
+    video_path = "video.mp4"
+
+    # Display the video
+    st.video(video_path)
+
     df = load_data()
     song_id_to_title = pd.Series(df.title.values, index=df.song).to_dict()
 
